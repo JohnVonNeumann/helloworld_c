0c26263472e78ae3e35b8622c6f92cc36e325e77
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-22 15:30:49 +1100

    first commit, working through ANSI C

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>                    /* include information about std lib*/
+
+main()                                /* define a fucntion named main that recieves
+                                        no arg values */
+
+{                                     /* statements of main are encolsed in braces */
+  printf("hello, world\n");           /* main calls lib function printf to print this */
+}                                     /* sequence of chars; /n = newline char*/  
